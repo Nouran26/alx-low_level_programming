84dfc79ef6c4b310e718e5a6222c0501434799d6
Nouran26 <Nouran.hany26@gmail.com>
2023-04-27 00:39:05 -0700

    0-main.c

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*main - tests function that prints if intger is positivr or negative
+*Return: 0
+*/
+int main(void)
+{
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+}
